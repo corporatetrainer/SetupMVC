856f331df0fcd1fac86460f251651f2fc2f2ea47
corporatetrainer <corporatetrainer21@gmail.com>
2019-07-25 08:03:02 +0530

    Add project files.

---
+++ AddMVC1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AddMVC1
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        //public string Index()
+        //{
+        //    return "Coming from index";
+        //}
+
+        public JsonResult Index()
+        {
+            return Json(new { msg = "Coming from index"});
+        }
+    }
+}
